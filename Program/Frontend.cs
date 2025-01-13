5c120ed72e7867004e7a0eb54e66af63fa2bf9d2
dominik tasior <d.tasior5@gmail.com>
2025-01-13 15:34:50 +0100

    13.01.2025

---
+++ Program/Frontend.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_Uczelnia.Program
+{
+    public static class Frontend
+    {
+        public static void PokazEkranPowitalny()
+        {
+          
+        }
+    }
+}
--- Program/Program.cs
+++ Program/Program.cs
@@ -1,5 +1,56 @@
-﻿
+﻿using System.ComponentModel.Design;
+using System.Runtime.CompilerServices;
+using Zadanie_Uczelnia.Program;
+using static System.Net.Mime.MediaTypeNames;
+string login, pass;
 
 
-PokazEkranPowitalny();
+Console.WriteLine("Witaj w programie bankowym");
+Console.WriteLine("Wybierz co chcesz zrobić:");
+Console.WriteLine("1. Logowanie");
+Console.WriteLine("2. Rejestracja");
+Console.WriteLine("3. Wyjście");
+var wybor=Convert.ToInt32(Console.ReadLine());
+if (wybor >= 1 && wybor <= 3)
+{
+
+    switch (wybor)
+    {
+        case 1:
+            Console.Clear();
+            Console.Write("Login:");
+            login = Console.ReadLine();
+            Console.Write("Hasło:");
+            pass = Console.ReadLine();
+            if (login == "admin" && pass == "admin")
+            {
+                Console.WriteLine("Udało ci się zalogować");
+            }
+            else
+            {
+                Console.WriteLine("Błędne hasło lub login");
+            }
+            break;
+        case 2:
+            Console.Clear();
+            Console.Write("Podaj login:");
+            login = Console.ReadLine();
+            Console.Write("Podaj hasło:");
+            pass = Console.ReadLine();
+            Console.WriteLine("Zarejestrowano pomyślnie");
+
+            break;
+
+        case 3:
+            Console.ReadLine();
+            Environment.Exit(0);
+            break;
+
+
+    }
+}
+else
+{ Console.WriteLine("Niestety wcisnąłeś zły klawisz"); }
+
+
 
