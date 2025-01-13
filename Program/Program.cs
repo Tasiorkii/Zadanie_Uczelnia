a75c743fddfcb21b500858f2e28dc9743fbc1945
dominik tasior <d.tasior5@gmail.com>
2025-01-13 15:32:12 +0100

    Początkowa wersja aplikacji bankowej 13.01.2025

--- Program/Program.cs
+++ Program/Program.cs
@@ -1,8 +1,5 @@
 ﻿
 
-Console.WriteLine("Witaj w programie bankowym");
-Console.WriteLine("Wybierz co chcesz zrobić:");
-Console.WriteLine("1. Logowanie");
-Console.WriteLine("2. Rejestracja");
-Console.WriteLine("3. Wyjście");
-var wybor = Console.ReadLine();
+
+PokazEkranPowitalny();
+
