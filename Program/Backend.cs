d8dd2a882f739dcb369dea1d60be63d27e8e946a
dominik tasior <d.tasior5@gmail.com>
2025-01-14 11:27:39 +0100

    14.01.2025

--- Program/Backend.cs
+++ Program/Backend.cs
@@ -7,7 +7,70 @@ using System.Threading.Tasks;
 namespace Zadanie_Uczelnia.Program
 {
     internal class Backend
-    {   
+    {
         
+        public static void Wybor()
+        {
+           
+            var wybor = Convert.ToInt32(Console.ReadLine());
+            if (wybor >= 1 && wybor <= 3)
+            {
+
+                switch (wybor)
+                {
+                    case 1:
+                        LogIn();
+                    break;
+                    case 2:
+                        SignIn();   
+                    break;
+
+                    case 3:
+                        Console.ReadLine();
+                        Environment.Exit(0);
+                    break;
+
+
+                }
+            }
+            else
+            { Console.WriteLine("Niestety wcisnąłeś zły klawisz"); }
+            
+
+        }
+        
+        public static void LogIn()
+        {
+            string mail, pass;
+            Console.Clear();
+            Console.Write("Login:");
+            mail = Console.ReadLine();
+            Console.Write("Hasło:");
+            pass = Console.ReadLine();
+            if (mail == "admin" && pass == "admin")
+            {
+                Console.WriteLine("Udało ci się zalogować");
+            }
+            else
+            {
+                Console.WriteLine("Błędne hasło lub login");
+            }
+        }
+       
+        public static void SignIn()
+        {
+            string mail, pass;
+            Console.Clear();
+            Console.Write("Podaj e-mail:");
+            Console.Write("Podaj hasło:");
+            pass = Console.ReadLine();
+            
+            
+            
+            Console.WriteLine("Zarejestrowano pomyślnie");
+
+        }
+
+
     }
 }
--- Program/Frontend.cs
+++ Program/Frontend.cs
@@ -10,7 +10,15 @@ namespace Zadanie_Uczelnia.Program
     {
         public static void PokazEkranPowitalny()
         {
-          
+                Console.WriteLine("Witaj w programie bankowym");
+                Console.WriteLine("Wybierz co chcesz zrobić:");
+                Console.WriteLine("1. Logowanie");
+                Console.WriteLine("2. Rejestracja");
+                Console.WriteLine("3. Wyjście");
+                
+
         }
     }
+    
+    
 }
