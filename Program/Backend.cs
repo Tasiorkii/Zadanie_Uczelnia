008d890ec513882667d0a73c5ee882a7e907358c
kamarov360 <kamarov360@gmail.com>
2025-02-01 15:50:02 +0100

    Dodano pętlę przy logowaniu, jeżeli użytkownik zostanie niezalogowany będzie mógł ponownie wpisać dane logowania, pokaże się również stosowny komunikat o nieudanej operacji.

--- Program/Backend.cs
+++ Program/Backend.cs
@@ -30,23 +30,32 @@ namespace Zadanie_Uczelnia.Program
             else
             { Console.WriteLine("Niestety wcisnąłeś zły klawisz"); }
         }
+
         public static void LogIn()
         {
             string mail, pass;
-            Console.Clear();
-            Console.Write("Login:");
-            mail = Console.ReadLine();
-            Console.Write("Hasło:");
-            pass = Console.ReadLine();
-            if (mail == "admin" && pass == "admin")
+            bool authorization = false;//zmienna do weryfikacji czy użytkownik jest prawidłowo zalogowany
+            while (!authorization)//pętla, wykonuje się zawsze jeżeli zmienna authorization jest zawsze na false
             {
-                Console.WriteLine("Udało ci się zalogować");
-            }
-            else
-            {
-                Console.WriteLine("Błędne hasło lub login");
+                Console.Clear();
+                Console.Write("Login:");
+                mail = Console.ReadLine();
+                Console.Write("Hasło:");
+                pass = Console.ReadLine();
+
+                if (mail == "admin" && pass == "admin")
+                {
+                    Console.WriteLine("Udało ci się zalogować");
+                    authorization = true;
+                }
+                else
+                {
+                    Console.WriteLine("Błędne hasło lub login");
+                    Thread.Sleep(3000);//Sleep aby użytkownik na 3 sekundy zobaczył że niepoprawnie został zalogowany
+                }
             }
         }
+
         public static void SignIn()
         {
             string mail, pass;
